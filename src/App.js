ce62650432e185acf8ee69b8112294ff7752bd23
Kinsley Alexander <kinsleyalexander@Kinsleys-MacBook-Air.local>
2023-10-22 20:36:49 -0400

    Finish bootstrap practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ApplicationNavbar from './Components/Navbar';
+import Item from './Components/Item';
 import './App.css';
 
-
-
 function App() {
+
   return (
-    <div className="App">
-      <h1>Bootstrap Website</h1>
-     
+    <div>
+      <ApplicationNavbar />
+      <Container fluid>
+        <Row>
+          {[1,1,1,1,1,1,1,1,1,1,1,1].map((item, i) => {
+            return (
+              <Col xs='12' md='6' lg='4' className='mb-2'>
+                <Item key={i} />
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
